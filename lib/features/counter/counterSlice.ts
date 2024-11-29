4f93a412bcf8ba85db0cf0ecd1aa4a988120b491
chocoscoding <chocoscoding@gmail.com>
2024-11-29 16:37:41 +0100

    init

--- lib/features/counter/counterSlice.ts
+++ lib/features/counter/counterSlice.ts
@@ -31,11 +31,9 @@ export const counterSlice = createAppSlice({
       state.value -= 1;
     }),
     // Use the `PayloadAction` type to declare the contents of `action.payload`
-    incrementByAmount: create.reducer(
-      (state, action: PayloadAction<number>) => {
-        state.value += action.payload;
-      },
-    ),
+    incrementByAmount: create.reducer((state, action: PayloadAction<number>) => {
+      state.value += action.payload;
+    }),
     // The function below is called a thunk and allows us to perform async logic. It
     // can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
     // will call the thunk with the `dispatch` function as the first argument. Async
@@ -43,6 +41,8 @@ export const counterSlice = createAppSlice({
     // typically used to make async requests.
     incrementAsync: create.asyncThunk(
       async (amount: number) => {
+        console.log("called");
+
         const response = await fetchCount(amount);
         // The value we return becomes the `fulfilled` action payload
         return response.data;
@@ -58,7 +58,7 @@ export const counterSlice = createAppSlice({
         rejected: (state) => {
           state.status = "failed";
         },
-      },
+      }
     ),
   }),
   // You can define your selectors here. These selectors receive the slice
@@ -70,8 +70,7 @@ export const counterSlice = createAppSlice({
 });
 
 // Action creators are generated for each case reducer function.
-export const { decrement, increment, incrementByAmount, incrementAsync } =
-  counterSlice.actions;
+export const { decrement, increment, incrementByAmount, incrementAsync } = counterSlice.actions;
 
 // Selectors returned by `slice.selectors` take the root state as their first argument.
 export const { selectCount, selectStatus } = counterSlice.selectors;
